5b89d619a613ed10ca6781e7336ef0111b5e7368
Le Quang Huy <huylequang2701@gmail.com>
2022-03-17 16:10:16 +0700

    Learn Tab navigation

--- TrainingReactNative/index.js
+++ TrainingReactNative/index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/controllers/App';
 import {name as appName} from './app.json';
+import StackNav from './src/navigation/StackNavigation';
+import TabNav from './src/navigation/TabNavigation';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TabNav);
